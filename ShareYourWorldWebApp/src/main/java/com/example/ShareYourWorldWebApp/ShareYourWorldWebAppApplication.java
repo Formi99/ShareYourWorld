15cf43efe5a8e898ea809e1eea3e373e9bbf3d42
micpar97 <m.paradiso@itsrizzoli.it>
2020-04-08 14:20:35 +0200

    progetto

---
+++ ShareYourWorldWebApp/src/main/java/com/example/ShareYourWorldWebApp/ShareYourWorldWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShareYourWorldWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareYourWorldWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareYourWorldWebAppApplication.class, args);
+	}
+
+}
