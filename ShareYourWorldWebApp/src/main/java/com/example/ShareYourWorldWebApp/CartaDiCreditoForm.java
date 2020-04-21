e9a64159ae43deea8f14eb5a0f71669c143d639b
micpar97 <m.paradiso@itsrizzoli.it>
2020-04-21 16:39:23 +0200

    Aggiunte le classi carta di credito e carta di credito form

---
+++ ShareYourWorldWebApp/src/main/java/com/example/ShareYourWorldWebApp/CartaDiCreditoForm.java
@@ -0,0 +1,54 @@
+package com.example.ShareYourWorldWebApp;
+
+import java.sql.Date;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+public class CartaDiCreditoForm {
+	
+	@Size (min=5, max=50)
+	private String datiProprietario;
+	@Size (min=13, max=16)
+	private String numero;
+	@Min(3)
+	@Max(3)
+	private int cv;
+	private Date dataScadenza;
+	
+	public String getDatiProprietario() {
+		return datiProprietario;
+	}
+
+	public void setDatiProprietario(String datiProprietario) {
+		this.datiProprietario = datiProprietario;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public int getCv() {
+		return cv;
+	}
+
+	public void setCv(int cv) {
+		this.cv = cv;
+	}
+
+	public Date getDataScadenza() {
+		return dataScadenza;
+	}
+
+	public void setDataScadenza(Date dataScadenza) {
+		this.dataScadenza = dataScadenza;
+	}
+
+	
+
+}
